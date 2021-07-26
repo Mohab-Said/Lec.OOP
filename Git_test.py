2b027528bc5fda3e7abdc4fbb71f72ff087d50c5
Mohab-Said <mohab.25@outlook.com>
2021-07-26 21:00:22 +0200

    Thanos Project ...
    We had a universe which was containing 50 images represent the whole universe.
    
    We removed randomly half of the universe using python libraries to make sure we have the power of all stones  together ;)

--- Git_test.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello to the first line in a func")
-
-print("Test this new line on Github")
-
-print("Last push test line with ctrl+shift+k shortcut")
---
+++ Thanos.py
@@ -0,0 +1,25 @@
+import os, random
+from random import sample
+
+#Checking dir to see if we have to change it to the targeted one
+path= 'E:\\Epsilon AI (CDSP)\\16-07-2021\\GCR\\Thanos Project\\Universe2'
+dir_check= os.getcwd()
+print("Current path is: ",dir_check)
+
+#Changing current dir to the one we want to read our files from
+os.chdir(path)
+dir_check= os.getcwd()
+print('New path is: ',dir_check)
+
+#Checking how many files in this folder using (len())
+files_check= os.listdir(path)
+print('You have: ', len(files_check))
+
+#Removing half of the universe random
+'''
+for files in sample(files_check,25):
+    os.remove(files)
+'''
+
+#Checking how many files is now in the universe
+print('Now you have {} files'.format(len(files_check)))
